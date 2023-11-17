d701c8d74592a0d26ed670fa7fb636d66c96832e
Salvador Lopez <salvador.lopez@tactos.com.mx>
2023-11-16 18:45:52 -0600

    Primer commit

---
+++ src/components/MuySaludableAdminApp.jsx
@@ -0,0 +1,6 @@
+
+export const MuySaludableAdminApp = () => {
+  return (
+    <div>MuySaludableAdminApp</div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { MuySaludableAdminApp } from './components/MuySaludableAdminApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MuySaludableAdminApp />
+  </React.StrictMode>,
+)
